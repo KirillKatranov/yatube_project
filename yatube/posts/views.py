960070959de0c4941ca2d29e33df884db5e0d871
Kirill Katranov <katranovrpk@gmail.com>
2021-09-19 10:46:20 +0300

    Добавил паджинатор и декоратор на авторизацию

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .models import Post, Group
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
-    text = 'Это главная страница проекта Yatube'
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'text' : text,
-        'posts' : posts
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
+@login_required
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     text = 'Здесь будет информация о группах проекта Yatube'
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -32,6 +32,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
+    'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -65,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.year.year',
             ],
         },
     },
@@ -121,5 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+LOGOUT_REDIRECT_URL = 'posts:index' 
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails') 
 
 
